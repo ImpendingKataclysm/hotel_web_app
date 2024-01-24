cd1b745ebd8bbc8dc8ab26431f16e153622315a8
ImpendingKataclysm <ktjanzen42@gmail.com>
2024-01-24 14:25:32 -0800

    Update the date input fields to display a calendar from which a user may only select valid dates

--- frontend/src/components/BookingModal.jsx
+++ frontend/src/components/BookingModal.jsx
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import {DatePicker} from "reactstrap-date-picker";
 import {
     Modal,
     ModalHeader,
@@ -9,10 +10,42 @@ import {
     Input, Button,
 } from "reactstrap";
 
-// Display the booking form for a selected hotel
+/*
+Display the booking form for a selected hotel, ensuring that the user may only
+select valid booking dates (i.e. they cannot select a start date earlier than the
+current date, and they cannot select an end date earlier than the start date)
+*/
 class BookingModal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            startDate: new Date(),
+            endDate: new Date(),
+        }
+    }
+
+    // Set the booking start date and end date to the user's selected start date
+    handleStartDateChange = (date) => (
+        this.setState({
+            startDate: date,
+            endDate: date,
+        })
+    );
+
+    // Set the booking end date to the user's selected end date
+    handleEndDateChange = (date) => (
+        this.setState({
+            endDate: date,
+        })
+    );
+
+    // Get the date (yyyy-mm-dd) from the datetime ISO string
+    getDateFromISO = (dateString) => dateString.split('T')[0];
+
     render() {
         const {toggle, hotel, save} = this.props;
+        const {startDate, endDate} = this.state;
 
         return (
             <Modal isOpen={true} toggle={toggle}>
@@ -27,8 +60,8 @@ class BookingModal extends Component {
                             customer_email: e.target.customer_email.value,
                             location: hotel.id,
                             booked_spa: e.target.book_spa ? e.target.book_spa.checked : false,
-                            start_date: e.target.start_date.value,
-                            end_date: e.target.end_date.value,
+                            start_date: this.getDateFromISO(e.target.start_date.value),
+                            end_date: this.getDateFromISO(e.target.end_date.value),
                         });
                     }}>
                         <FormGroup>
@@ -52,20 +85,26 @@ class BookingModal extends Component {
                         </FormGroup>
                         <FormGroup>
                             <Label for={"start_date"}>Date Arriving:</Label>
-                            <Input
+                            <DatePicker
                                 type={"date"}
                                 id={"start_date"}
                                 name={"start_date"}
                                 required={true}
+                                value={startDate}
+                                minDate={new Date()}
+                                onChange={(startDate) => this.handleStartDateChange(startDate)}
                             />
                         </FormGroup>
                         <FormGroup>
                             <Label for={"end_date"}>Date Departing:</Label>
-                            <Input
+                            <DatePicker
                                 type={"date"}
                                 id={"end_date"}
                                 name={"end_date"}
                                 required={true}
+                                value={endDate}
+                                minDate={startDate}
+                                onChange={(endDate) => this.handleEndDateChange(endDate)}
                             />
                         </FormGroup>
                         {hotel.has_spa ?
